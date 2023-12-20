fa58a5a78575b139641132df601b7d9aaaefcc2e
Aoun Developer <aoundeveloper@gmail.com>
2023-12-20 21:19:18 +0500

    day 4

--- Day-4/app.js
+++ Day-4/app.js
@@ -4,17 +4,19 @@
 function regularfunction() {
     document.write("I am Regular Function" + "<br>")
 }
-regularfunction()
+// regularfunction()
+
 // 2- Function Expression
 const functionExpression = function () {
     document.write("I am Function Expression" + "<br>")
 }
-functionExpression()
+// functionExpression()
+
 // 3- Arrow Function
 const arrowfunction = () => {
     document.write("I am Arrow Function" + "<br>")
 }
-arrowfunction()
+// arrowfunction()
 
 // Returning a value
 const addfunc = (a, b) => {
@@ -22,7 +24,7 @@ const addfunc = (a, b) => {
     return ans;
 
 }
-let result = document.write(addfunc(4, 5 + "<br>"))
+// let result = document.write(addfunc(4, 5 + "<br>"))
 
 const calcpod = function (products, tax) {
     let total = 0;
@@ -34,18 +36,25 @@ const calcpod = function (products, tax) {
 };
 
 let productsANS = calcpod([20, 30, 40], 0.2)
-console.log(productsANS);
+// console.log(productsANS);
 
 // Set Timeout function
-setTimeout (() => {
+setTimeout(() => {
     let employees = ["John", "Mike", "Peter"];
-    console.log(employees);
-},2000);
+    // console.log(employees);
+}, 2000);
 
 let box = document.querySelector("#box");
 
 setTimeout(() => {
-    box.style.height = "200px";
+    // box.style.height = "200px";
 }, 3000)
 
 // ForEach Method
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+days.forEach(function (days) {
+    console.log(days)
+    console.log(typeof days)
+    document.write(days+ "<br>");
+})
