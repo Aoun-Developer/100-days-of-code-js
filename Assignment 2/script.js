7eceeda9f1604947fc1a61260f1ec0366049106f
Aoun Developer <aoundeveloper@gmail.com>
2023-12-24 13:08:42 +0500

    question 8

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -57,7 +57,7 @@ const palindrome = (data) => {
     }
 }
 
-let result4 = palindrome("hello");
+// let result4 = palindrome("hello");
 // document.write("4- Check if hello is a palindrome: <br>" + result4)
 
 const polindrome = (data) => {
@@ -68,5 +68,49 @@ const polindrome = (data) => {
         return false;
     }
 }
-let result5 = polindrome("hello");
-document.write("5- Check if hello is a palindrome: <br>" + result5)
+// let result5 = polindrome("hello");
+// document.write("5- Check if hello is a palindrome: <br>" + result5)
+
+
+// 5. Question: Find the sum of all positive numbers in an array.
+
+const sum_of_arr = (...item) => {
+    let element = 0;
+    for (const key of item) {
+        element += key
+    }
+    return element
+}
+
+// document.write(sum_of_arr(10,10,5,20,5));
+
+
+// 6. Question: Find the index of the first occurrence of a specific element in an array.
+
+const find_func = (arr, ele) => {
+    return arr.indexOf(ele);
+}
+
+const numb = [1, 3, 4, 6, 8, 9, 2, 2, 6, 4, 4, 1, 5, 9]
+const element = 9;
+
+// document.write(find_func(numb, element))
+
+// 7. Question: Remove all duplicates from an array without built-in methods.
+
+const remove_duplicates = (arr) => {
+    const uniqueArr = [];
+
+    for (const i in arr) {
+        if (uniqueArr.indexOf(arr[i]) === -1) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+
+const fruits = ["apple", "orange", "banana", "grapes", "apple"];
+document.write(remove_duplicates(fruits))
+
+// 8. Question: Sort the array in ascending and descending without built-in methods.
+
---
+++ temp.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "banana", "orange","apple","grapes"];
+
+let text1 = fruits.push();
+
+document.write(text1, "<br>");
+
+
+// clouser
