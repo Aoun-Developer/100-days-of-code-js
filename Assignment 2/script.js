53afc146e86df2472e66de7936d3b647d431614d
Aoun Developer <aoundeveloper@gmail.com>
2023-12-24 17:33:07 +0500

    aaa
    bbbb

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -110,7 +110,127 @@ const remove_duplicates = (arr) => {
 }
 
 const fruits = ["apple", "orange", "banana", "grapes", "apple"];
-document.write(remove_duplicates(fruits))
+// document.write(remove_duplicates(fruits))
 
 // 8. Question: Sort the array in ascending and descending without built-in methods.
 
+const bubbleSortAscending = (arr) => {
+    let len = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        len--;
+    } while (swapped);
+
+    return arr;
+};
+
+const bubbleSortDescending = (arr) => {
+    let len = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        len--;
+    } while (swapped);
+
+    return arr;
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// document.write(bubbleSortAscending(arr) + "<br>")
+// document.write(bubbleSortDescending(arr))
+
+// 9. Question: Print all even numbers between 1 and 20 using a while loop.
+let num = 2;
+
+while (num <= 20) {
+    // document.write(num + " ");
+    num += 2;
+}
+
+// 10. Question: Calculate the factorial of a number using a do-while loop.
+
+const factorial = (num) => {
+    let result = 1;
+    do {
+        result *= num;
+        num--;
+    } while (num > 0);
+    return result;
+}
+
+// document.write(factorial(5))
+
+// 11. Question: Iterate through the properties of an object using a for-in loop.
+
+const obj = {
+    name: "John",
+    age: 30
+};
+
+for (const key in obj) {
+    // document.write(key + ": " + obj[key] + "<br>");
+}
+
+// 12. Question: Loop through an array using a for-of loop and double each element.
+
+const arr2 = [1, 2, 3, 4, 5];
+
+for (const element of arr2) {
+    // document.write(element * 2 + " ");
+}
+
+// 13. Question: Check if a number is even or odd and return a corresponding message.
+const checkEvenOrOdd = (num) => {
+    if (num % 2 === 0) {
+        return "The number is even";
+    } else {
+        return "The number is odd";
+    }
+}
+
+// document.write(checkEvenOrOdd(5)+ "<br>"); 
+// document.write(checkEvenOrOdd(10)); 
+
+// 14. Question: Find the maximum of three numbers using nested ternary operators.
+const findMax = (num1, num2, num3) => {
+    return num1 > num2 ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+}
+
+// Example usage:
+const max = findMax(5, 10, 3);
+// document.write(max); 
+
+// 15. Question: Determine if a year is a leap year or not.
+const isLeapYear = (year) => {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// document.write(isLeapYear(2020) + " ");
+// document.write(isLeapYear(2021) + " ");
+// document.write(isLeapYear(2022) + " ");
+// document.write(isLeapYear(2023) + " ");
+// document.write(isLeapYear(2000) + " ");
+// document.write(isLeapYear(1900) + " "); 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WD_2.0-Javascript
- 
+
+5- Assingment II (15 questions)
+4- Day-5 Class Codes
+3- Day-3 Class Codes
+2- Day-2 Class Codes
+1- Day-1 Class Codes
