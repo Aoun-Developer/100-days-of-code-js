71ad4a0f2b0102de308cb1264f13e82daec77097
Aoun Developer <aoundeveloper@gmail.com>
2024-01-02 15:24:21 +0500

    class 3 done conditional and comparision done

--- CLASS/Day-3/app.js
+++ CLASS/Day-3/app.js
@@ -1,29 +1,82 @@
-// For Loop
-const cars = ["Mehran","civic","audi","bmw"];
+// the for loop
+
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
 
 for (let i = 0; i < cars.length; i++) {
-    const element = cars[i] + "<br>";
-    document.write(element)
+    document.write(i + ": " + cars[i] + "  ");
+}
+document.write("<br>")
+// While Loop
+
+let w = 50;
+while (w < 55) {
+    document.write(w + " ");
+    w++;
 }
 
-const cars2 = ["Mehran","civic","audi","bmw"];
+document.write("<br>")
+// Do while Loop
 
-for (let i = 0; i < cars2.length;) {
-    const element = cars2[i] + "<br>";
-    i++;
-    document.write(element)
-}
-
-// while loop
-let i = 0;
-while (i < 0) {
-    // console.log(i);
-    i++;
-}
-
-// Do while loop
-let index = 0;
+let d = 70;
 do {
-    // console.log(index);
-    index++;
-} while (index < 0)
+    document.write(d + " ");
+    d++;
+}
+while (d <= 70);
+
+document.write("<br>")
+// If Statement
+
+let age = "Are You A Male";
+
+if (age == "yes") {
+    document.write("You are a Male");
+} else {
+    document.write("You are a female");
+}
+
+document.write("<br>")
+
+let email = ("Enter Email")
+if (email.length > 12 && email.includes("@") && email.includes(".")) {
+    document.write("Valid Email");
+} else if (!email.length > 0 || typeof email != "string") {
+    document.write("not a email")
+} else {
+    document.write("Invalid Email");
+}
+
+document.write("<br>")
+// switch statement
+let gender = ("Are you a male");
+switch (gender) {
+    case "yes":
+    case "ya":
+    case "y":
+    case "i am":
+    case "yes i am":
+        document.write("You are a male");
+        break;
+
+    case "no":
+    case "nope":
+    case "nah":
+    case "i am not":
+        document.write("You are a female");
+        break;
+    default:
+        document.write("You are AI");
+}
+
+document.write("<br>")
+// ternary operator
+let voteable = 18;
+
+document.write(voteable >= 18 ? "Eligible" : "Not eligible");
+
+document.write("<br>")
+// Nullish Coalescing Operator
+let namee = prompt("enter");
+let text = "missing";
+let result = namee ?? text;
+document.write(result);
