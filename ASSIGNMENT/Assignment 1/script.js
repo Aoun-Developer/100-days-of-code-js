780a51af2d3a93432834f903f2784f8a18f56c91
Aoun Developer <aoundeveloper@gmail.com>
2024-01-03 15:15:56 +0500

    assigment 2 problems completed Q/A remaining

--- ASSIGNMENT/Assignment 1/script.js
+++ ASSIGNMENT/Assignment 1/script.js
@@ -8,6 +8,18 @@ const reverse_str = (data) => {
 }
 // document.write(reverse_str("Hello"));
 
+
+
+
+
+
+
+
+
+
+
+
+
 // 2. Question: Count the number of vowels in a given string.
 const vowel_checker = (data) => {
     let x = "";
@@ -21,6 +33,18 @@ const vowel_checker = (data) => {
 }
 // document.write(vowel_checker("Hello"));
 
+
+
+
+
+
+
+
+
+
+
+
+
 // 3. Question: Convert the first letter of each word in a sentence to uppercase.
 
 const upperCase = (data) => {
@@ -30,6 +54,18 @@ const upperCase = (data) => {
 }
 
 // document.write(upperCase("hello world"));
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 4. Question: Check if a string is a palindrome.
 
@@ -44,6 +80,18 @@ const check_palindrome = (data) => {
     return x;
 }
 // document.write(check_palindrome("madam"));
+
+
+
+
+
+
+
+
+
+
+
+
 // 5. Question: Find the sum of all positive numbers in an array.
 const sum_of_array = (data) => {
     let sum = 0;
@@ -56,27 +104,216 @@ const sum_of_array = (data) => {
 }
 let array = [0, 1, -2, -4, 1, -4, 3, 4, 3, -34, -52];
 // document.write(sum_of_array(array));
+
+
+
+
+
+
+
+
+
+
+
+
 // 6. Question: Find the index of the first occurrence of a specific element in an array.
 const find_index = (arr, target) => {
     return arr.indexOf(target);
 }
 const names = ["z", "b", "c", "d", "a", "b", "c", "d", "a", "b", "c", "d"];
-document.write(find_index(names, "a"));
+// document.write(find_index(names, "a"));
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 7. Question: Remove all duplicates from an array without built-in methods.
+const remove_duplicate = (data) => {
+    let unique_array = [];
+    for (const key in data) {
+        if (unique_array.indexOf(data[key]) === -1) {
+            unique_array.push(data[key]);
+        }
+    }
+    return unique_array;
+}
+
+const name_1 = names;
+// document.write(remove_duplicate(name_1));
+
+
+
+
+
+
+
+
+
+
+
+
 
 // 8. Question: Sort the array in ascending and descending without built-in methods.
 
+
+
+
+
+
+
+
+
+
+
+
+
 // 9. Question: Print all even numbers between 1 and 20 using a while loop.
 
+const check_even = (start, end) => {
+    let x = start;
+    let z = "";
+    while (x <= end) {
+        if (x % 2 === 0) {
+            z += x + " ";
+        }
+        x++;
+    }
+    return z;
+}
+
+// document.write(check_even(1, 20));
+
+
+
+
+
+
+
+
+
+
+
 // 10. Question: Calculate the factorial of a number using a do-while loop.
+const factorial = (data) => {
+    let initial = 1;
+    let result = 1;
+
+    do {
+        result *= initial;
+        initial++;
+
+    } while (initial <= data);
+
+    return result;
+}
+
+// document.write(factorial(3));
+
+
+
+
+
+
+
+
+
+
+
 
 // 11. Question: Iterate through the properties of an object using a for-in loop.
 
+const obj = {
+    name: "aoun",
+    age: 20,
+    location: "karachi",
+
+}
+
+for (const key in obj) {
+    // document.write(key + " : " + obj[key] + "<br>")
+}
+
+
+
+
+
+
+
+
+
+
 // 12. Question: Loop through an array using a for-of loop and double each element.
+
+const double = (data) => {
+    let new_arr = [];
+    for (const key of data) {
+        new_arr.push(key * 2);
+    }
+    return new_arr;
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// document.write(double(arr));
+
+
+
+
+
+
+
+
+
+
 
 // 13. Question: Check if a number is even or odd and return a corresponding message.
 
+const num_checker = (data) => {
+    if (data % 2 === 0) {
+        return `${data} is even`;
+    }
+    else {
+        return `${data} is odd`;
+    }
+}
+// document.write(num_checker(45));
+
+
+
+
+
+
+
+
+
+
+
 // 14. Question: Find the maximum of three numbers using nested ternary operators.
 
+const max = (a, b, c) => {
+    return (a >= b) ? ((a >= c) ? (a) : (c)) : ((b >= c) ? (b) : (c)) + " is maximum";
+}
+
+// document.write(max(1, 4, 3));
+
+
+
+
+
+
+
+
+
+
 // 15. Question: Determine if a year is a leap year or not.
+const leap_check = (year) => {
+    return (year % 4 === 0 && (year % 100 === 0 || year % 400 === 0)) ? true : false;
+}
+
+// document.write(leap_check(2000));
---
+++ ASSIGNMENT/Assignment 2/app.js
@@ -0,0 +1,285 @@
+// 1. Rewrite the following code using a ternary operator:
+// let result;
+// if (score >= 80) {
+//     result = "Pass";
+// } else {
+//     result = "Fail";
+// }
+
+const score = (data) => {
+    return data >= 80 ? "Pass" : "fail";
+}
+
+// document.write(score(70));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2. How does the optional chaining operator (?.) work, and how can it be used to access nested properties of an object?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3. Compare the for...in loop and the for...of loop in terms of their use cases and the types of values they iterate over.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 4. Define a function calculateAverage that takes an array of numbers as an argument and returns the average value.
+
+const avg_value = (data) => {
+    let sum = 0;
+    data.forEach(elm => sum += elm);
+    return sum / data.length;
+};
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// document.write(avg_value(values));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5. Explain the concept of "closures" in JavaScript and provide an example of their practical use.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 6. Create an object named student with properties name, age, and grades. Add a method calculateAverage that calculates the average of the grades.
+
+const obj = {
+    name: "Aoun",
+    age: 24,
+    grades: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    calculateAverage: () => {
+        let sum = 0;
+        obj.grades.forEach(elm => sum += elm);
+        return sum / obj.grades.length;
+    },
+    calculateAverage_1: function () {
+        let sum = 0;
+        this.grades.forEach(elm => sum += elm);
+        return sum / this.grades.length;
+    }
+
+}
+// document.write(obj.calculateAverage());
+// document.write("<br>");
+// document.write(obj.calculateAverage_1());
+
+
+
+
+
+
+
+
+
+
+
+
+// 7. How can you clone an object in JavaScript and also give one example each deep copy, shallow copy, and reference copy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 8. Write a loop that iterates over an array of numbers and logs whether each number is even or odd, using a ternary operator.
+
+const even_odd = (data) => {
+    let x = [];
+    data.forEach((elm) => {
+        x.push(elm % 2 === 0 ? `${elm} is even` : `${elm} is odd`);
+    });
+    return x;
+}
+let numb = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// document.write(even_odd(numb));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 9. Describe the differences between the for loop, while loop, and do...while loop in JavaScript. When might you use each?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 10. Provide an example of using optional chaining within a loop to access a potentially missing property of an object.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 11. Write a for...in loop that iterates over the properties of an object and logs each property name and value.
+const obj_1 = {
+    name: "Aoun",
+    age: 24,
+    grades: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    fees: 100,
+    fees_status: "paid",
+    course: "Mern stack",
+    course_duration: 3
+}
+for (const key in obj_1) {
+    // document.write(`Property is (${key}) : Value is (${obj_1[key]}) <br>`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 12. Explain the use of the break and continue statements within loops. Provide scenarios where each might be used.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 13. Write a function calculateTax that calculates and returns the tax amount based on a given income. Use a ternary operator to determine the tax rate.
+
+const calculateTax = (income, tax) => income * tax / 100;
+
+// document.write(calculateTax(80000, 3));
+
+
+
+
+
+
+
+
+
+
+
+
+// 14. Create an object car with properties make, model, and a method startEngine that logs a message. Instantiate the object and call the method.
+
+const car_obj = {
+    make: "Toyota",
+    model: "Camry",
+    startEngine: function () {
+        return (`Starting the engine of ${this.make} ${this.model}`)
+    }
+}
+
+// document.write(car_obj.startEngine());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 15. Explain the differences between regular functions and arrow functions in terms of scope, this binding, and their use as methods.
